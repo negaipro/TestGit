b2a14d8a13f4e6584a548cda5c6562c906f27a80
하정웅 <admin@admin-ui-iMac.local>
2013-07-01 18:58:49 +0900

    Initial Commit

---
+++ TestGit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestGit
+//
+//  Created by 하정웅 on 13. 7. 1..
+//  Copyright (c) 2013년 negaipro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
